9ec16db5606201f19dc46707c8287718ffff7f98
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-10-03 22:00:00 -0300

    created the featured button component

---
+++ src/components/buttons/FeaturedButton/index.tsx
@@ -0,0 +1,20 @@
+
+import * as S from './styled'
+import { ButtonProps } from '../../../types';
+
+export default function FeaturedButton({ children }: ButtonProps) {
+
+    return (
+        <S.Button
+            
+            onClick={() => {}}
+        >
+
+
+            {
+                children
+            }
+
+        </S.Button>
+    )
+}
---
+++ src/components/buttons/FeaturedButton/styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+        align-items: center;
+        background-color: #FFFFFF;
+
+        border-width: 1px;
+        border-style: solid;
+        border-color: #D9D9D9;
+
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+        cursor: pointer;
+        /* width: 188px; */
+        /* height: 40px; */
+        min-width: fit-content;
+        min-height: fit-content;
+        border-radius: 10px;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        position: relative;
+
+        align-self: flex-start;
+
+        padding: 10px 43px;
+
+
+        justify-content: center;
+   
+        padding: 10 20PX;
+        color: #383838;
+        font-size: 20px;
+        
+        
+
+    `
