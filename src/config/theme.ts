4946e89d165a8da2cf37b5b222484c92dd6b0525
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-06 13:59:15 -0300

    created the theme setting in config

---
+++ src/config/theme.ts
@@ -0,0 +1,17 @@
+
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    black: "#000000",
+    darkGray: "#383838",
+    button: { normal: "#ed6b47" },
+  },
+  font: {
+    family: "Poppins",
+    weigths: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+};
