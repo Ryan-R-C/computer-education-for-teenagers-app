2d1eb17b2067a6b879ed4ed3d764aeaacbdb16d7
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-13 15:53:07 -0300

    created the task controller provider

---
+++ src/contexts/TaskControllerProvider.tsx
@@ -0,0 +1,72 @@
+import { createContext, useState, useEffect, useContext } from "react";
+// import FitCultural from "service/fitCultural/fitCultural";
+
+import { ReactProps, QuestionProps, TaskProps } from "../types";
+
+export const TaskControllerContext = createContext({});
+
+export default function TaskControllerProvider({ children }: ReactProps) {
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean>(false);
+  const [currentQuestion, setCurrentQuestion] = useState<TaskProps[]>();
+  const [currentTask, setCurrentTask] = useState<QuestionProps[]>();
+  const [errors, setErrors] = useState(0);
+  const [hits, setHits] = useState(0);
+
+
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
+
+
+  async function findFitCultural(id: string) {
+    try {
+      setLoadingSubmit(true);
+      // const selectedFitCultural = await FitCultural.find(id);
+      const selectedFitCultural = {};
+
+      return selectedFitCultural;
+    } catch (e) {
+      console.log("error", e);
+      setLoadingSubmit(false);
+    }
+  }
+
+  return (
+    <TaskControllerContext.Provider
+      value={{
+        findFitCultural,
+        isCorrectAnswer,setIsCorrectAnswer,
+        loadingSubmit, setLoadingSubmit,
+        currentQuestion, setCurrentQuestion,
+        currentTask, setCurrentTask,
+        errors, setErrors,
+        hits, setHits,
+      }}
+    >
+      {children}
+    </TaskControllerContext.Provider>
+  );
+}
+
+
+export function useTaskController() {
+  const context = useContext(TaskControllerContext);
+
+  const {
+    findFitCultural,
+    isCorrectAnswer,setIsCorrectAnswer,
+    loadingSubmit, setLoadingSubmit,
+    currentQuestion, setCurrentQuestion,
+    currentTask, setCurrentTask,
+    errors, setErrors,
+    hits,   setHits,
+  }: any = context;
+
+  return {
+    findFitCultural,
+    isCorrectAnswer,setIsCorrectAnswer,
+    loadingSubmit, setLoadingSubmit,
+    currentQuestion, setCurrentQuestion,
+    currentTask, setCurrentTask,
+    errors, setErrors,
+    hits,   setHits,
+  };
+}
