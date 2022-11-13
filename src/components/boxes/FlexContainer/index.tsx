e4dcaa6ec4fdb8f92f9f1b82399b86b268869ee7
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-13 16:02:19 -0300

    created the flex container component

---
+++ src/components/boxes/FlexContainer/index.tsx
@@ -0,0 +1,17 @@
+
+import * as S from './styled'
+import { ButtonProps } from '../../../types';
+
+
+export default function FlexContainer({ children }: ButtonProps) {
+
+    return (
+        <S.FlexContainer
+            
+        >
+            {
+                children
+            }
+        </S.FlexContainer>
+    )
+}
