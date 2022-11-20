d66e32e2082ac46b5c720a635f0915230d87bf4f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:10:06 -0300

    fixed the title's font-size

--- src/components/typography/Title/styled.ts
+++ src/components/typography/Title/styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-        font-size: 32px;
+        font-size: 20px;
         margin: 0;
         font-weight: normal;
     `
