656c6018417813b45c6bb7f8e4ead3bc3d7f635a
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 14:53:14 -0300

    connected tasks explanation's page with backend

--- src/pages/TaskExplanation/index.tsx
+++ src/pages/TaskExplanation/index.tsx
@@ -1,6 +1,6 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import * as S from "./LandingPage.styled";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import WelcomeContainer from '../../components/boxes/WelcomeContainer';
 import Title from '../../components/typography/Title';
@@ -10,33 +10,36 @@ import DefaultButtonEmphasis from "../../components/buttons/DefaultButtonEmphasi
 import Bar from "../../components/boxes/Bar";
 import HeaderBar from "../../components/boxes/header/HeaderBar";
 import MoreIcon from "../../components/icons/MoreIcon";
+import { useSubTasks } from "../../contexts/SubTaskProvider";
 
 export default function TaskExplanation( {}) {
-  const [step, setStep] = useState(0);
 
+  const {
+    subTasks,
+    setSubTask,
+    findSubTask,
+  } = useSubTasks()
 
-  const stepsMessages = {
-      title: "Como Surgiu a Internet?",
-      description: [
-        `Como surgiu a internet? A ARPANet (Rede de Agencias de  Projetos de Pesquisa Avançada ou Rede da Agência de Projetos de Pesquisa Avançada dos Estados Unidos), uma rede de comunicação de computadores com finalidades militares surgiu no ano de em...,
-        1969. A ideia era conectar várias universidades para a troca de informações entre elas, tanto para fins de comunicação, a comunicação entre computadores era muito mais rápida que uma carta,
-         quanto para segurança, caso houvesse algum acidente com a máquina a infomação ainda existiria em outros computadores`,
-      ],
+  const { _id } = useParams()
 
-    }
+  async function handleLoadSubTask(_id){
+    const currentSubTask = await findSubTask(_id)
 
-    const url = `http://www.pedromigao.com.br/ourodetolo/wp-content/uploads/2011/06/arpanet.jpg`
+    setSubTask(currentSubTask)
+  }
 
-
-  
+  useEffect(
+    () => {
+      handleLoadSubTask(_id)
+    }, [_id]
+  )
 
 
   return (
     <S.Container>
       <S.ImageContainer
       style={{
-        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.5) 100%), url(${url})`
-
+        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.5) 100%), url(${subTasks?.image})`
       }}
       >
         <HeaderBar>
@@ -47,41 +50,35 @@ export default function TaskExplanation( {}) {
       </HeaderBar>
         
       </S.ImageContainer>
-      <>
-       
-            <S.WelcomeHolder>
-              <div className="content">
-                <Title>
-                  {
-                    stepsMessages.title
-                  }
-                </Title>
-                <p>
-                  {
-                    stepsMessages.description[step]
-                  }
-                </p>
-              </div>
-              <S.ButtonsContainer>
-                <S.StepContainer>
-                {stepsMessages.description.map((x, i) =>
-                  <Step
-                  key={i}
-                  current={step === i}
-                  onClick={() => setStep(i)}
-                  />
-                )}
-                </S.StepContainer>
-                <S.GetStartedContainer>
-                  <Link to="/task/:id">
-                    <DefaultButtonEmphasis>
-                      Ir para lição
-                    </DefaultButtonEmphasis>
-                  </Link>
-                </S.GetStartedContainer>
-              </S.ButtonsContainer>
-            </S.WelcomeHolder>
-      </>
+        <S.WelcomeHolder>
+          <div className="content">
+            <Title>
+              {
+                subTasks?.title
+              }
+            </Title>
+            {
+              subTasks?.content?.map(
+                (content, index) => (
+                  <p key={index}>
+                    {
+                      content
+                    }
+                  </p>
+                )
+              )
+            }
+          </div>
+          <S.ButtonsContainer>
+            <S.GetStartedContainer>
+              <Link to={`/task/${subTasks?._id}`}>
+                <DefaultButtonEmphasis>
+                  Ir para lição
+                </DefaultButtonEmphasis>
+              </Link>
+            </S.GetStartedContainer>
+          </S.ButtonsContainer>
+        </S.WelcomeHolder>
     </S.Container>
 
   );
