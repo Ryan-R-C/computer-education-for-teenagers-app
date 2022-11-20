5e5514a083bb38893c453a2a09e545822a41467d
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-11-20 15:09:42 -0300

    updated the providers

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,13 @@ import TaskProvider from "./contexts/TaskProvider";
 import SubTaskProvider from "./contexts/SubTaskProvider";
 import QuestionProvider from "./contexts/QuestionProvider";
 import OptionsProvider from "./contexts/OptionsProvider";
+import UsersProvider from "./contexts/UsersProvider";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import TaskExplanation from "./pages/TaskExplanation";
 import Tasks from "./pages/Tasks";
 import Test from "./pages/Test";
+import UserScoresProvider from "./contexts/UserScoresProvider";
 
 export const footerProps = [
   {
@@ -31,20 +33,24 @@ const RoutesApp = () => {
             <SubTaskProvider>
               <QuestionProvider>
                 <OptionsProvider>
-                  <TaskControllerProvider>
-                    <BrowserRouter>
-                      <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/dashboard/" element={<Dashboard />} />
-                        <Route path="/explanation-task/:_id" element={<TaskExplanation />} />
-                        <Route path="/task/:_id" element={<Tasks />} />
-                        <Route path="/test/" element={<Test />} />
-                        {/*
-                        <Route path="/cadastro" element={<SignUp />} /> */}
-                  
-                      </Routes>
-                    </BrowserRouter>
-                  </TaskControllerProvider>
+                  <UsersProvider>
+                    <UserScoresProvider>
+                      <TaskControllerProvider>
+                        <BrowserRouter>
+                          <Routes>
+                            <Route path="/" element={<LandingPage />} />
+                            <Route path="/dashboard/" element={<Dashboard />} />
+                            <Route path="/explanation-task/:_id" element={<TaskExplanation />} />
+                            <Route path="/task/:_id" element={<Tasks />} />
+                            <Route path="/test/" element={<Test />} />
+                            {/*
+                            <Route path="/cadastro" element={<SignUp />} />
+                            */}
+                          </Routes>
+                        </BrowserRouter>
+                      </TaskControllerProvider>
+                    </UserScoresProvider>
+                  </UsersProvider>
                 </OptionsProvider>
               </QuestionProvider>
             </SubTaskProvider>
